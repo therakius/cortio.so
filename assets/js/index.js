87137ad8c417f2bec1db7b2b12b6b17db546adb5
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-04-25 18:10:09 +0200

    fixed: result now appears only after sending the link

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,11 +4,11 @@ const displayFeedbackForm = document.getElementById('tofeedback')
 const feedbackForm = document.getElementById('feedback')
 const feedbackFormExit = document.querySelector('#feedback-exit')
 const feedbackLinkButton = document.getElementById('tofeedback')
-
+const onSubmit = document.getElementById('submit-btn')
 const toShare = document.querySelector('#share');
 const popup = document.getElementById('share-popup');
-
 const exitShare = document.getElementById('exit')
+const resultForm = document.getElementById('result-form');
 
 
 exitShare.addEventListener("click", ()=>{
@@ -27,6 +27,7 @@ toCopy.addEventListener('click', async ()=>{
          await navigator.clipboard.writeText(link);
          toCopy.innerHTML = 'copied <i class="ph ph-check-fat"></i>';
          toCopy.style.color = "var(--success-color)";
+
          setTimeout(()=>{
             toCopy.innerHTML = 'copy <i class="ph ph-clipboard-text"></i>'
             toCopy.style.color = "black";
@@ -49,5 +50,15 @@ displayFeedbackForm.addEventListener('click', (event)=>{
 feedbackFormExit.addEventListener('click', ()=>{
     feedbackForm.classList.add('inactive')
     feedbackLinkButton.style.color = 'black'
+})
+
+onSubmit.addEventListener('click', (event)=>{
+    event.preventDefault()
+
+    resultForm.classList.remove('hidden')
+    setTimeout(() => {
+        resultForm.classList.add('hidden')
+    }, 15000);
+    
 })
 
