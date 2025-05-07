b29fb8eeb3c0e9ad11ba898252a092e29fb0eb5f
Gaspar Mario Chichava <gasparc.chichava@gmail.com>
2025-05-07 18:33:23 +0200

    updated get &  post request logic

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config(); 
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,12 +17,39 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static("public"));
-app.set("view engine", "ejs");
+app.set("view engine", "html");
 app.use(express.json());
 
 app.get('/', async (req, res)=>{
-    res.render('index.ejs')
+    res.sendFile(__dirname+ '/views/index.html')
 })
+
+const link = 'https://api.tinyurl.com/create';
+
+const config = {
+  headers: {
+    Authorization: `Bearer ${process.env.API_TOKEN}`,
+    'Content-Type': 'application/json',
+  },
+};
+
+app.post('/submit', async (req, res) => {
+  const userUrl = req.body.userUrl;
+
+  try {
+    const response = await axios.post(link, { url: userUrl }, config);
+    
+    const shortenedLink = response.data.data.tiny_url;
+    console.log(response.data);
+
+    res.json( {link: shortenedLink});
+
+  } catch (error) {
+    console.error(error.response?.data || error.message);
+    res.status(500).json({ error: 'Erro ao encurtar o link' });
+  }
+});
+
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`)
